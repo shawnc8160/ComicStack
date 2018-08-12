8f71ddabb904d6a0bf2e791d9c8a1ab2fd8f4b03
Joshua Payne <Intellijosh@MacBook-Pro.local>
2018-08-11 22:22:59 -0600

    sending with query routes in backend

---
+++ app/models/query.rb
@@ -0,0 +1,17 @@
+class Query
+
+    # ==================================================
+    #                      ROUTES
+    # ==================================================
+    require 'net/http'
+
+
+    # get one by id
+    def self.find(opts)
+
+      return opts[url]
+
+    end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   patch  '/user/:id'       => 'users#update'
   delete '/user/:id'       => 'users#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get    '/queries/:url'        => 'queries#find'
 end
