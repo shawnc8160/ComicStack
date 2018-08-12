fa9119d7dce65e3304b6c45d45451553b578dd67
Joshua Payne <Intellijosh@MacBook-Pro.local>
2018-08-11 23:31:09 -0600

    query route returns jsons on browser

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module Searchstack
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.action_dispatch.default_headers = {
-    'Access-Control-Allow-Origin' => 'http://my-web-service-consumer-site.com',
+    'Access-Control-Allow-Origin' => '*',
     'Access-Control-Request-Method' => %w{GET POST OPTIONS}.join(",")
   }
 
