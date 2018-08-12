93f7cc9a2985336d46a0faa25164829ea2aaf003
shawnc8160 <shawnc8160@gmail.com>
2018-08-12 14:49:39 -0700

    Sessions is all done

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,3 @@
-
 class App extends React.Component {
   constructor(props) {
     super(props)
@@ -8,12 +7,47 @@ class App extends React.Component {
     }
     this.sendResults = this.sendResults.bind(this)
     this.setUser = this.setUser.bind(this)
+    this.getCookieData = this.getCookieData.bind(this)
+  }
+  /*=======================
+  Things to check for when page first loads
+  =======================*/
+  componentDidMount () {
+    // Check to see if user is logged in
+    if (this.state.user == null) {
+      // Check for cookie information if they aren't logged in
+      this.getCookieData()
+    }
+  }
+  /*=======================
+  Gets user data from cookies
+  =======================*/
+  getCookieData() {
+    console.log('Checking for cookies');
+    if(Cookies.get('token') && Cookies.get('id') && Cookies.get('username')) {
+      console.log('token exists');
+      // Format user data
+      let userData = {
+        username: Cookies.get('username'),
+        id: Cookies.get('id'),
+        token: Cookies.get('token')
+      }
+      // load user data into state
+      this.setState({
+        user: userData
+      })
+    } else {
+      console.log('token does not exist');
+    }
   }
   sendResults(data) {
     this.state.searchResults
   }
+  /*=======================
+  Sets user data in state and cookies after log in
+  =======================*/
   setUser(userdata, token) {
-    userdata['tokenString'] = token;
+    userdata['token'] = token;
     console.log('data for user is: ', userdata);
     // Sets userdata into cookie
     Cookies.set('token', token);
@@ -28,7 +62,7 @@ class App extends React.Component {
     return (
       <div className='section'>
         <h1> ComicStack </h1>
-        <LoginForm setUser={this.setUser}/>
+        {(this.state.user == null) ? <LoginForm setUser={this.setUser}/> : <div>Logged in as {this.state.user.username}</div>}
         <SearchForm sendResults={this.sendResults}/>
       </div>
     )
