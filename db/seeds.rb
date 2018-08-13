786ae14c6457deb244f92c1cacc1ffb9422cd559
Joshua Payne <Intellijosh@MacBook-Pro.local>
2018-08-13 13:05:23 -0600

    favorite, character tables added to seeds file, Favorite component started

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,10 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
-# Examples:
-create table likes (id serial, user_id int, character_id int);
+#likes table
+create table favorites (id serial, user_id int, character_id int);
+#characters table
+create table characters (id int, name varchar(60), deck text, description text, publisher varchar(40), gender int, icon_url varchar(200), real_name varchar(80));
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
---
+++ public/js/favorite.js
@@ -0,0 +1,16 @@
+class Favorite extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+
+  }
+
+  render() {
+    return (
+      <div>
+      </div>
+    )
+  }
+}
